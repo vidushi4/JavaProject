1630f6529ca5903707914756097a90a7effe084b
Vidushi Agrawal <vidushi.agrawal2015@vit.ac.in>
2018-09-14 00:13:05 +0530

    Update Project.java
    Updated the values in panel and call repaint so that the simulation is natural. Added multi threading to make the animation efficient. Added java class.

--- src/Project.java
+++ src/Project.java
@@ -1,144 +1,256 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*; 
 
-public class Project implements ActionListener
+class Jug
 {
-    JTextField tf1,tf2,tf3;
-    JTextArea area;
-    JugPanel panel;
-    JButton b1;
+	int capacity;
+	int value;
+	Jug(int n)
+	{
+		capacity = n;
+		value = 0;
+	}
+	void Fill()
+	{
+		value = capacity;
+	}
+	void Empty()
+	{
+		value = 0;
+	}
+	boolean isFull()
+	{
+		return value >= capacity;
+	}
+	boolean isEmpty()
+	{
+		return value == 0;
+	}
+	
+	int getValue()
+	{
+	return value;
+	}
+	
+}	
+public class Projectfinal extends Frame implements ActionListener
+{  
+    TextField tf1,tf2,tf3;  
+	TextArea area;
+	int h1=0,h2=0;
+    Button b1,b2;  
+	Projectfinal()
+	{  
+        super("Water Jug");
+        tf1=new TextField("Capacity of jug 1");  
+        tf1.setBounds(50,50,150,20);  
+        tf2=new TextField("Capacity of jug 2");  
+        tf2.setBounds(230,50,150,20);  
+        tf3=new TextField("Required capacity");  
+        tf3.setBounds(410,50,150,20);  
+        
+        b1=new Button("Submit");  
+        b1.setBounds(590,50,80,20); 
+		
+		area=new TextArea();  
+        area.setBounds(50,400, 200,320); 
+		
+		b1.addActionListener(this);  
+          
+        add(tf1);add(tf2);add(tf3);add(b1); add(area); 
+        setSize(1200,1200);  
+        setLayout(null);  
+        setVisible(true);  
+    }  
+		
+	void drawing(Graphics g)
+	{
+		Graphics2D graphics = (Graphics2D) g;
+        graphics.setPaint(Color.BLUE);
+		
+        		
+		System.out.println("inside drawing");
+		this.update(g);
+       
+	   
+		graphics.fillRect(100, 300-h1,99,h1);
+		graphics.fillRect(300, 300-h2,99,h2);
+       
+	   
+	   graphics.setPaint(Color.BLACK);
 
-    Timer timer;
+        graphics.setStroke(new BasicStroke(3));
 
-    Project()
-    {
-        JFrame frame= new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 500);
-        frame.setResizable(true);
-        frame.setLocationRelativeTo(null);
-        frame.setLayout(new BorderLayout());
-        JPanel pane = new JPanel();
-        pane.setLayout(new GridBagLayout());
+        	
+        graphics.drawLine(100, 150, 100, 300);
+        graphics.drawLine(100, 300, 200, 300);
+        graphics.drawLine(200, 150, 200, 300);
+		
+		
+		graphics.drawLine(300, 150, 300, 300);
+        graphics.drawLine(300, 300, 400, 300);
+        graphics.drawLine(400, 150, 400,300);
+		
+	}
+	public void paint(Graphics g)
+	{
+        Graphics2D graphics = (Graphics2D) g;
+        graphics.setPaint(Color.BLUE);
+		
+       	
+		System.out.println("inside paint");
+		
+			
+        graphics.fillRect(100, 300-h1,99,h1);
+		graphics.fillRect(300, 300-h2,99,h2);
+        graphics.setPaint(Color.BLACK);
 
-        tf1 = new JTextField("Capacity of jug 1", 10);
-        tf2 = new JTextField("Capacity of jug 2", 10);
-        tf3 = new JTextField("Required capacity", 10);
-        panel = new JugPanel();
-        b1 = new JButton("Submit");
-        area = new JTextArea(5, 30);
+        graphics.setStroke(new BasicStroke(3));
 
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(5, 20, 5, 20);
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        pane.add(tf1, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        pane.add(tf2, gbc);
-        gbc.gridx = 2;
-        gbc.gridy = 0;
-        pane.add(tf3, gbc);
-        gbc.gridx = 3;
-        gbc.gridy = 0;
-        pane.add(b1, gbc);
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        gbc.gridwidth = 4;
-        gbc.fill = GridBagConstraints.BOTH;
-        pane.add(panel, gbc);
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        pane.add(area, gbc);
+       	
+        graphics.drawLine(100, 150, 100, 300);
+        graphics.drawLine(100, 300, 200, 300);
+        graphics.drawLine(200, 150, 200, 300);
+		
+		
+		graphics.drawLine(300, 150, 300, 300);
+        graphics.drawLine(300, 300, 400, 300);
+        graphics.drawLine(400, 150, 400,300);
+		
+		Thread t= Thread.currentThread();
+		try
+		{
+			
+			t.sleep(500);
+		}
+		catch(Exception e)
+		{
+			System.out.println("exception found ");
+		}		
 
-        frame.add(new JScrollPane(pane), BorderLayout.CENTER);
-        frame.setVisible(true);
+	}	
+	
+	public void actionPerformed(ActionEvent e) 
+	{  
+        String s1=tf1.getText();  
+        String s2=tf2.getText();
+		String s3=tf3.getText();
+        int a=Integer.parseInt(s1);  
+        int b=Integer.parseInt(s2);  
+        int result=Integer.parseInt(s3);  
+		
+		Jug A=new Jug(a);
+		Jug B=new Jug(b);
+	
+		area.setText("(A,B) = (" + A.getValue()+ ","+B.getValue()+")"+"\n");
+		
+		Thread t= Thread.currentThread();
+		while(A.getValue() != result)
+		{
+			System.out.println("in");
+			if(!A.isFull() && B.isEmpty())
+			{
+				area.append("Fill B"+"\n");
+				B.Fill();
+				h1=A.getValue()*10;
+				h2=B.getValue()*10;
+				Graphics g =this.getGraphics();
+				drawing(g);
+					try{
+					t.sleep(1000);
+					}
+					catch(Exception e1)
+					{
+						System.out.println("Hello world");
+					}
+				
+				area.append( "(A, B) = (" + A.getValue() + ", " + B.getValue() +")"+"\n");
+				repaint();
+			}
+			if(A.isFull())
+			{
+				area.append( "Empty A"+"\n");
+				A.Empty();
+				
+				h1=A.getValue()*10;
+				h2=B.getValue()*10;
+				Graphics g =this.getGraphics();
+				
+				drawing(g);
+				
+				try
+				{
+					t.sleep(1000);
+					}
+					catch(Exception e1)
+					{
+						System.out.println("Hello world");
+					}
+			area.append("(A, B) = (" + A.getValue()+ ", "+B.getValue()+ ")"+"\n");
+				repaint();
+				
+			}
+			area.append( "Pour from B into A"+"\n");
+			fill(A, B);
+			
+			h1=A.getValue()*10;
+			h2=B.getValue()*10;
+			System.out.println("h1 ,h2 = "+ h1 +" " +h2);
+			Graphics g =this.getGraphics();
+			
+			drawing(g);
+			try{
+				t.sleep(1000);
+				}
+				catch(Exception e1)
+				{
+					System.out.println("Hello world");
+				}
+		area.append( "(A, B) = (" + A.getValue() + ", " + B.getValue() + ")"+"\n");
+			repaint();
+		}
+		
+		area.append("Done!"+"\n");
+		
+    }  
+	static void fill(Jug A, Jug B)
+	{
+	int old_value = A.value;
 
-        b1.addActionListener(this);
-        timer = new Timer(1000,this);
-    }
-    public void actionPerformed(ActionEvent e)
-    {
-        if(e.getSource() == b1) {
-            String s1 = tf1.getText();
-            String s2 = tf2.getText();
-            String s3 = tf3.getText();
-            int a = Integer.parseInt(s1);
-            int b = Integer.parseInt(s2);
-            int result = Integer.parseInt(s3);
+	A.value = A.value + B.value;
+	A.value = A.value > A.capacity?A.capacity:A.value;
 
-            Jug A = new Jug(a);
-            Jug B = new Jug(b);
+	B.value = B.value - (A.value - old_value);
+	}
+	static int gcd(int n,int m)
+	{ 
+		if(m<=n && n%m == 0)
+			return m;
+		if(n < m)
+			return gcd(m,n);
+		else
+			return gcd(m,n%m);
+	}
+	static boolean check(int a,int b,int c)
+	{
+		if(c>a)
+		{
+			System.out.println("A can't hold more water than it's capacity!");
+			return false;
+		}
+ 		if(c % gcd(a,b) == 0)
+		{
+			return true;
+		}
+ 		System.out.println("Can't reach this state with the given jugs");
+		return false;
+	}
 
-            area.setText("(A,B) = (" + A.getValue() + "," + B.getValue() + ")" + "\n");
-
-            while (A.getValue() != result) {
-                if (!A.isFull() && B.isEmpty()) {
-                    area.append("Fill B" + "\n");
-                    B.Fill();
-                    area.append("(A, B) = (" + A.getValue() + ", " + B.getValue() + ")" + "\n");
-                }
-                if (A.isFull()) {
-                    area.append("Empty A" + "\n");
-                    A.Empty();
-                    area.append("(A, B) = (" + A.getValue() + ", " + B.getValue() + ")" + "\n");
-                }
-                area.append("Pour from B into A" + "\n");
-                fill(A, B);
-                area.append("(A, B) = (" + A.getValue() + ", " + B.getValue() + ")" + "\n");
-            }
-
-            area.append("Done!" + "\n");
-
-            //Save steps to be performed and start timer here. Use code: timer.start();
-        } else {
-            /*This block is executed when the timer is running. The timer will call
-            this block every second...you can change this by changing the delay during
-            initialization. Implement simulation logic here...ie how to update the
-            values in panel and call repaint so that the simulation is natural. My advice
-            is to disable the button while simulation is running. Don't forget to stop the
-            timer and enable it once the simulation is over though.
-            Use code: timer.stop(); b1.setEnabled(true/false)*/
-        }
-
-    }
-    static void fill(Jug A, Jug B)
-    {
-        int old_value = A.value;
-
-        A.value = A.value + B.value;
-        A.value = A.value > A.capacity?A.capacity:A.value;
-
-        B.value = B.value - (A.value - old_value);
-    }
-    static int gcd(int n,int m)
-    {
-        if(m<=n && n%m == 0)
-            return m;
-        if(n < m)
-            return gcd(m,n);
-        else
-            return gcd(m,n%m);
-    }
-    static boolean check(int a,int b,int c)
-    {
-        if(c>a)
-        {
-            System.out.println("A can't hold more water than it's capacity!");
-            return false;
-        }
-        if(c % gcd(a,b) == 0)
-        {
-            return true;
-        }
-        System.out.println("Can't reach this state with the given jugs");
-        return false;
-    }
-
-
-    public static void main(String[] args) {
-        new Project();
-    }
-}
+	
+public static void main(String[] args) 
+{  
+    new Projectfinal();  
+	
+} 
+}  
